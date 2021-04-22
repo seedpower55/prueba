7bb007bb146141ad2c9b2cdc1f0a0a98d38b97c7
DiiegoFG <82582890+DiiegoFG@users.noreply.github.com>
2021-04-22 12:31:44 +0200

    añadir una variable
    he añadido una variable al proyecto de jorge

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         TODO code application logic here
     }
-    
+    N=4
 }
